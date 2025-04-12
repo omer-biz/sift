a81ea0da03d83b1b42bc7d63bb9a3a35697bc751
omer <omerabdi@protonmail.com>
2025-04-12 10:54:17 +0300

    feat: editor component

---
+++ src/Components/Editor.elm
@@ -0,0 +1,240 @@
+module Components.Editor exposing (Model, Msg(..), init, new, subscriptions, update, view)
+
+import Dict exposing (Dict)
+import Effect exposing (Effect)
+import Html exposing (..)
+import Html.Attributes exposing (class, placeholder, type_, value)
+import Html.Events exposing (onBlur, onClick, onFocus, onInput, onSubmit)
+import Json.Decode as D
+import Time
+import Types.Note exposing (Note)
+import Types.Tag as Tag exposing (Tag)
+import Utils
+
+
+type Editor msg
+    = Settings
+        { model : Model
+        , toMsg : Msg -> msg
+        , onSubmit : Note -> msg
+        }
+
+
+new :
+    { model : Model, toMsg : Msg -> msg, onSubmit : Note -> msg }
+    -> Editor msg
+new props =
+    Settings
+        { model = props.model
+        , toMsg = props.toMsg
+        , onSubmit = props.onSubmit
+        }
+
+
+newNote : Note
+newNote =
+    { title = ""
+    , content = ""
+    , tags = []
+    , createdAt = Time.millisToPosix 0
+    , updatedAt = Time.millisToPosix 0
+    }
+
+
+type alias Model =
+    { note : Note
+    , tagQuery : String
+    , tagSugg : List Tag
+    , tags : Dict Int Tag
+    , tagInputFocus : Bool
+    }
+
+
+init : { a | note : Maybe Note } -> Model
+init opts =
+    { note = Maybe.withDefault newNote opts.note
+    , tagQuery = ""
+    , tagSugg = []
+    , tags = Dict.empty
+    , tagInputFocus = False
+    }
+
+
+type Msg
+    = UpdateField Field String
+    | GotTags (Result D.Error (List Tag))
+    | AddTagSugg Tag
+    | RemoveTag Tag
+    | ToggleTagInput Bool
+    | Cancel
+    | NoOp
+
+
+type Field
+    = Content
+    | TagQuery
+
+
+update :
+    { model : Model
+    , toModel : Model -> model
+    , msg : Msg
+    , toMsg : Msg -> msg
+    }
+    -> ( model, Effect msg )
+update props =
+    let
+        model =
+            props.model
+
+        toParentModel ( innerModel, effect ) =
+            ( props.toModel innerModel, effect )
+    in
+    toParentModel <|
+        let
+            note =
+                model.note
+        in
+        case props.msg of
+            UpdateField Content value ->
+                ( { model
+                    | note =
+                        { note | content = value }
+                  }
+                , Effect.none
+                )
+
+            UpdateField TagQuery value ->
+                if String.trim value == "" then
+                    ( { model | tagQuery = value, tagSugg = [] }, Effect.none )
+
+                else
+                    ( { model | tagQuery = value }, Effect.getTags value )
+
+            GotTags (Ok tags) ->
+                ( { model | tagSugg = tags }, Effect.none )
+
+            GotTags (Err err) ->
+                let
+                    _ =
+                        Debug.log "decode tags error: " err
+                in
+                ( model, Effect.none )
+
+            AddTagSugg tag ->
+                let
+                    dictTags =
+                        Dict.insert tag.id tag model.tags
+                in
+                ( { model
+                    | tagQuery = ""
+                    , tags = dictTags
+                    , note =
+                        { note
+                            | tags =
+                                dictTags
+                                    |> Dict.toList
+                                    |> List.map Tuple.second
+                        }
+                  }
+                , Effect.none
+                )
+
+            RemoveTag tag ->
+                ( { model | tags = Dict.remove tag.id model.tags }, Effect.none )
+
+            ToggleTagInput value ->
+                ( { model | tagInputFocus = value }, Effect.none )
+
+            Cancel ->
+                ( model, Effect.back )
+
+            NoOp ->
+                ( model, Effect.none)
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Utils.receieve (D.list Tag.decode) GotTags Effect.recTags
+
+
+view : Editor msg -> Html msg
+view (Settings settings) =
+    let
+        model =
+            settings.model
+
+        note =
+            model.note
+
+        viewTagSuggs =
+            let
+                colorToStyle color =
+                    "bg-" ++ color ++ "-400"
+
+                viewSugg tag =
+                    li
+                        [ onClick <| (settings.toMsg << AddTagSugg) tag
+                        , class <| "px-4 py-2 cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700 dark:text-white flex "
+                        ]
+                        [ span [ class <| "w-2 mr-2 rounded " ++ colorToStyle tag.color ] [], text tag.name ]
+            in
+            if model.tagQuery /= "" then
+                ul [ class "absolute w-full bg-white-200 dark:bg-black-600 border dark:border-black-700 rounded-lg mt-1 shadow-lg " ] <|
+                    List.map viewSugg model.tagSugg
+
+            else
+                text ""
+
+        viewTags tags =
+            if not <| Dict.isEmpty tags then
+                let
+                    listTags =
+                        tags
+                            |> Dict.toList
+                            |> List.map Tuple.second
+                in
+                div [ class "flex flex-wrap gap-2 mt-2" ] <|
+                    List.map (\tag -> span [ onClick <| (settings.toMsg << RemoveTag) tag ] [ Tag.view tag ]) listTags
+
+            else
+                text ""
+    in
+    div [ class "my-4 w-full bg-white-100 dark:bg-black-500 rounded-xl shadow-sm" ]
+        [ form [ onSubmit <| settings.toMsg NoOp, class "grid md:min-w-[700px] gap-3 min-h-[60vh]" ]
+            [ -- note body
+              fieldset [ class "edit-mode px-4 pt-4 border-r dark:border-gray-700" ]
+                [ textarea
+                    [ class "w-full h-64 p-2 border border-black-300 dark:border-black-400 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-300 dark:bg-black-300 dark:text-white-100"
+                    , placeholder "Write your note here..."
+                    , onInput <| (settings.toMsg << UpdateField Content)
+                    , value note.content
+                    ]
+                    []
+                ]
+
+            -- tag query field
+            , fieldset [ class "px-4" ]
+                [ h2 [ class "text-lg font-semibold" ]
+                    [ text "Tags" ]
+                , viewTags model.tags
+                , div [ class "relative w-full" ]
+                    [ input
+                        [ class "mt-4 p-2 border border-black-300 dark:border-black-600 rounded-md w-full bg-white-100 dark:bg-black-300 text-black-500 dark:text-white-100 focus:outline-none focus:ring-1 focus:ring-blue-300"
+                        , placeholder "Add a tag and press Enter"
+                        , value model.tagQuery
+                        , type_ "text"
+                        , onInput <| (settings.toMsg << UpdateField TagQuery)
+                        , onFocus <| (settings.toMsg << ToggleTagInput) True
+                        , onBlur <| (settings.toMsg << ToggleTagInput) False
+                        ]
+                        []
+                    , viewTagSuggs
+                    ]
+                ]
+            , fieldset [ class "px-4 text-right space-x-4" ]
+                [ button [ onClick <| settings.toMsg Cancel, class "bg-red-100 hover:bg-red-200 rounded shadow px-2 py-1 text-white-100" ] [ text "Cancle" ]
+                , button [ onClick <| settings.onSubmit model.note, class "bg-blue-100 hover:bg-blue-200 rounded shadow px-2 py-1 text-white-100" ] [ text "Save" ]
+                ]
+            ]
+        ]
--- src/Pages/NewNote.elm
+++ src/Pages/NewNote.elm
@@ -1,20 +1,18 @@
 module Pages.NewNote exposing (Model, Msg, page)
 
+import Components.Editor as Editor
 import Dict exposing (Dict)
 import Effect exposing (Effect)
 import Html exposing (..)
 import Html.Attributes exposing (class, placeholder, type_, value)
-import Html.Events exposing (onBlur, onClick, onFocus, onInput)
-import Json.Decode as D
+import Html.Events exposing (onInput)
 import Layouts
 import Page exposing (Page)
 import Route exposing (Route)
 import Shared
-import Task
-import Time exposing (Posix)
-import Types.Note as Note exposing (Note)
-import Types.Tag as Tag exposing (Tag)
-import Utils
+import Time
+import Types.Note exposing (Note)
+import Types.Tag exposing (Tag)
 import View exposing (View)
 
 
@@ -31,7 +29,7 @@ page shared route =
 
 toLayout : Model -> Layouts.Layout Msg
 toLayout model =
-    Layouts.Scaffold { header = viewTitle model }
+    Layouts.Scaffold { header = viewTitle model.editor.note }
 
 
 
@@ -44,6 +42,7 @@ type alias Model =
     , tagSugg : List Tag
     , tags : Dict Int Tag
     , tagInput : Bool
+    , editor : Editor.Model
     }
 
 
@@ -64,15 +63,14 @@ initModel =
     , tagSugg = []
     , tags = Dict.empty
     , tagInput = False
+    , editor = Editor.init <| { note = Just newNote }
     }
 
 
 init : () -> ( Model, Effect Msg )
 init () =
     ( initModel
-    , Time.now
-        |> Task.perform TimeNow
-        |> Effect.sendCmd
+    , Effect.none
     )
 
 
@@ -81,80 +79,38 @@ init () =
 
 
 type Msg
-    = UpdateField Field String
-    | TimeNow Posix
-    | GotTags (Result D.Error (List Tag))
-    | AddTagSugg Tag
-    | RemoveTag Tag
-    | ToggleTagInput Bool
-
-
-type Field
-    = Content
-    | Title
-    | TagQuery
+    = CreateNote Note
+    | EditorSent Editor.Msg
+    | UpdateTitle String
 
 
 update : Msg -> Model -> ( Model, Effect Msg )
 update msg model =
-    let
-        note =
-            model.note
-    in
     case msg of
-        UpdateField Title value ->
-            ( { model
-                | note =
-                    { note | title = value }
-              }
-            , Effect.none
-            )
-
-        UpdateField Content value ->
-            ( { model
-                | note =
-                    { note | content = value }
-              }
-            , Effect.none
-            )
-
-        UpdateField TagQuery value ->
-            ( { model
-                | tagQuery = value
-              }
-            , if String.trim value == "" then
-                Effect.none
-
-              else
-                Effect.getTags value
-            )
-
-        TimeNow posix ->
-            ( { model
-                | note =
-                    { note | createdAt = posix, updatedAt = posix }
-              }
-            , Effect.none
-            )
-
-        GotTags (Ok tags) ->
-            ( { model | tagSugg = tags }, Effect.none )
-
-        GotTags (Err err) ->
+        CreateNote note ->
             let
                 _ =
-                    Debug.log "decode tags error: " err
+                    Debug.log "note" note
             in
-            ( model, Effect.none )
+            ( model, Effect.none)
 
-        AddTagSugg tag ->
-            ( { model | tagQuery = "", tags = Dict.insert tag.id tag model.tags }, Effect.none )
+        EditorSent innerMsg ->
+            Editor.update
+                { msg = innerMsg
+                , model = model.editor
+                , toModel = \editor -> { model | editor = editor }
+                , toMsg = EditorSent
+                }
 
-        RemoveTag tag ->
-            ( { model | tags = Dict.remove tag.id model.tags }, Effect.none )
+        UpdateTitle title ->
+            let
+                editor =
+                    model.editor
 
-        ToggleTagInput value ->
-            ( { model | tagInput = value }, Effect.none )
+                note =
+                    editor.note
+            in
+            ( { model | editor = { editor | note = { note | title = title } } }, Effect.none )
 
 
 
@@ -163,23 +119,24 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Utils.receieve (D.list Tag.decode) GotTags Effect.recTags
+    Editor.subscriptions model.editor
+        |> Sub.map EditorSent
 
 
 
 -- VIEW
 
 
-viewTitle : Model -> Html Msg
-viewTitle model =
+viewTitle : Note -> Html Msg
+viewTitle note =
     div [ class "flex justify-between items-center" ]
         [ div [ class "flex flex-col" ]
             [ input
                 [ class "text-xl w-64 font-bold bg-transparent border-0 focus:ring-0 placeholder-gray-500 px-1 ml-1 mt-1 focus:outline-none focus:ring-1 focus:ring-blue-300"
                 , placeholder "Note title..."
                 , type_ "text"
-                , value model.note.title
-                , onInput <| UpdateField Title
+                , value note.title
+                , onInput UpdateTitle
                 ]
                 []
             , div [ class "text-sm text-gray-500 px-2" ]
@@ -195,7 +152,7 @@ viewTitle model =
             ]
         , div [ class " px-4 flex items-center text-sm text-gray-500" ]
             [ span [ class "word-count" ]
-                [ model.note.content
+                [ note.content
                     |> String.words
                     |> List.filter (\w -> w /= "")
                     |> List.length
@@ -205,7 +162,7 @@ viewTitle model =
             , span [ class "mx-2" ]
                 [ text "•" ]
             , span [ class "character-count" ]
-                [ model.note.content
+                [ note.content
                     |> String.length
                     |> String.fromInt
                     |> (\count -> text <| count ++ " chars")
@@ -217,88 +174,12 @@ viewTitle model =
 view : Model -> View Msg
 view model =
     { title = "New Note"
-    , body = [ viewEditor model ]
-    }
-
-
-viewEditor : Model -> Html Msg
-viewEditor model =
-    let
-        note =
-            model.note
-    in
-    div [ class "my-4 w-full bg-white-100 dark:bg-black-500 rounded-xl shadow-sm" ]
-        [ form [ class "grid md:min-w-[700px] gap-3 min-h-[60vh]" ]
-            [ -- note body
-              fieldset [ class "edit-mode px-4 pt-4 border-r dark:border-gray-700" ]
-                [ textarea
-                    [ class "w-full h-64 p-2 border border-black-300 dark:border-black-400 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-300 dark:bg-black-300 dark:text-white-100"
-                    , placeholder "Write your note here..."
-                    , onInput <| UpdateField Content
-                    , value note.content
-                    ]
-                    []
-                ]
-
-            -- tag query field
-            , fieldset [ class "px-4" ]
-                [ h2 [ class "text-lg font-semibold" ]
-                    [ text "Tags" ]
-                , viewTags model.tags
-                , div [ class "relative w-full" ]
-                    [ input
-                        [ class "mt-4 p-2 border border-black-300 dark:border-black-600 rounded-md w-full bg-white-100 dark:bg-black-300 text-black-500 dark:text-white-100 focus:outline-none focus:ring-1 focus:ring-blue-300"
-                        , placeholder "Add a tag and press Enter"
-                        , value model.tagQuery
-                        , type_ "text"
-                        , onInput <| UpdateField TagQuery
-                        , onFocus <| ToggleTagInput True
-                        , onBlur <| ToggleTagInput False
-                        ]
-                        []
-                    , viewTagSuggs model
-                    ]
-                ]
-            , fieldset [ class "px-4 text-right space-x-4" ]
-                [ button [ class "bg-red-100 hover:bg-red-200 rounded shadow px-2 py-1 text-white-100" ] [ text "Cancle" ]
-                , button [ class "bg-blue-100 hover:bg-blue-200 rounded shadow px-2 py-1 text-white-100" ] [ text "Save" ]
-                ]
-            ]
+    , body =
+        [ Editor.new
+            { model = model.editor
+            , toMsg = EditorSent
+            , onSubmit = CreateNote
+            }
+            |> Editor.view
         ]
-
-
-viewTagSuggs : Model -> Html Msg
-viewTagSuggs model =
-    let
-        colorToStyle color =
-            "bg-" ++ color ++ "-400"
-
-        viewSugg tag =
-            li
-                [ onClick <| AddTagSugg tag
-                , class <| "px-4 py-2 cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700 dark:text-white flex "
-                ]
-                [ span [ class <| "w-2 mr-2 rounded " ++ colorToStyle tag.color ] [], text tag.name ]
-    in
-    if model.tagInput || model.tagQuery /= "" then
-        ul [ class "absolute w-full bg-white dark:bg-black-600 border dark:border-black-700 rounded-lg mt-1 shadow-lg " ] <|
-            List.map viewSugg model.tagSugg
-
-    else
-        text ""
-
-
-viewTags : Dict Int Tag -> Html Msg
-viewTags tags =
-    if not <| Dict.isEmpty tags then
-        let
-            listTags =
-                tags
-                    |> Dict.toList
-                    |> List.map Tuple.second
-        in
-        div [ class "flex flex-wrap gap-2 mt-2" ] <|
-            List.map (\tag -> span [ onClick <| RemoveTag tag ] [ Tag.view tag ]) listTags
-
-    else
-        text ""
+    }
